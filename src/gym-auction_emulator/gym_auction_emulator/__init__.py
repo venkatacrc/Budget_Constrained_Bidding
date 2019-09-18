d77ebc114c2d314c0843b3be187b3b1bac1a1da0
Venkata Chintapalli <venkatc77@gmail.com>
2019-09-18 10:25:40 -0700

    gym auction emulator skeleton added

---
+++ src/gym-auction_emulator/gym_auction_emulator/__init__.py
@@ -0,0 +1,1 @@
+from gym_auction_emulaor.envs.auction_emulator_env import AuctionEmulatorEnv
---
+++ src/gym-auction_emulator/gym_auction_emulator/envs/auction_emulator_env.py
@@ -0,0 +1,15 @@
+import gym
+from gym import error, utils
+from gym.utils import seeding
+
+class AuctionEmulatorEnv(gym.Env):
+    def __init__(self):
+        pass
+    def step(self, action):
+        pass
+    def reset(self):
+        pass
+    def render(self, mode='human'):
+        pass
+    def close(self):
+        pass
